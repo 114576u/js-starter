d1108156063dc3f87d58efcc061fb6071cc7da94
114576u <52041244+114576u@users.noreply.github.com>
2021-05-29 12:25:18 +0200

    Follow training Basic operations, keeping up with the video training

---
+++ src/first.js
@@ -0,0 +1,48 @@
+// freeCodeCamp Javascript tutorial
+
+var a = 4;
+/* Data Types */
+var myName = "Pepet";
+myName = 8;
+// let is only valid within the scope where it is declared
+let ourName = "freeCodeCamp";
+const pi = 3.14;
+console.log(a);
+
+var a = 5;
+var b = 10;
+var c = "i am a";
+a = a + 1;
+b = b + 5;
+c = c + " String!";
+console.log(c);
+
+var this_is_a_var = "this is a var";
+var THIS_IS_A_VAR = "THIS IS A VAR";
+var This_Is_A_Var = "This Is A Var";
+console.log(this_is_a_var);
+console.log(THIS_IS_A_VAR);
+console.log(This_Is_A_Var);
+
+// Adding numbers
+var sum = 10 + 10 + 1;
+console.log(sum);
+var subs = 10 - 40;
+console.log(subs);
+var quotient = 66 / 56;
+console.log(quotient);
+
+// increment
+var myVar = 30;
+myVar++;
+console.log(myVar);
+myVar--;
+console.log(myVar);
+var myDecimal = 3.4;
+myDecimal = myDecimal * 4.2;
+console.log(myDecimal);
+// remainder:
+var remainder;
+remainder = 11 % 3;
+console.log(remainder);
+
