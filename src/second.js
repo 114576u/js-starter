84e9ca69b8e332141f755fca1404ca9af7341cd6
114576u <52041244+114576u@users.noreply.github.com>
2021-05-29 23:11:31 +0200

    Updater

--- src/second.js
+++ src/second.js
@@ -110,3 +110,39 @@ var secondTree = myPlants[1].list;
 console.log(myPlants[1]);
 console.log(myPlants[1].list[1]);
 console.log(secondTree);
+console.log(secondTree[1]);
+
+var collection = {
+  "2548": {
+    "album": "Slippery When Wet",
+    "artist": "Bon Jovi",
+    "tracks": ["Let It Rock", "You Give Love a Bad Name"]
+  },
+  "2468": {
+    "album": "1999",
+    "artist": "Prince",
+    "tracks": ["1999", "Little Red Corvette"]
+  },
+  "1245": {
+    "artist": "Robert Palmer",
+    "tracks": []
+  },
+  "5439": {
+    "album": "ABBA Gold"
+  }
+};
+var collectionCopy = JSON.parse(JSON.stringify(collection));
+console.log(collection);
+console.log(collectionCopy);
+
+function updateRecords(id, prop, value) {
+  if (value === ""){
+    delete collection[id][prop];
+  } else if (prop === "tracks") {
+    collection[id][prop] = collection[id][prop] || [];
+    collection[id][prop].push(value);
+  }
+  return collection;
+}
+updateRecords(5439, "artist", "ABBA");
+
