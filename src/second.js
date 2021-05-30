ecfeeacec2b99a0a5dd47bb5962828a171add171
114576u <52041244+114576u@users.noreply.github.com>
2021-05-30 12:48:29 +0200

    Added multiplyAll function

--- src/second.js
+++ src/second.js
@@ -189,3 +189,15 @@ for (var i = 0; i < ourArray.length; i++){
 console.log(ourTotal);
 
 
+function mulitplyAll(arr) {
+  var product = 1;
+  for (var i = 0; i < arr.length; i++) {
+    for (var j = 0 ; j < arr[i].length; j++) {
+      product *= arr[i][j];
+    }
+  }
+  return product;
+}
+var product = mulitplyAll([[1, 2],[3, 4],[1, 2]]);
+console.log(product);
+
